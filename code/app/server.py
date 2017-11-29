97af08e4726e92bf67cb1a9041420fbca94089f7
Jonas Schumacher <jonas.merlin.schumacher@mailbox.org>
2017-11-29 22:20:37 +0100

    port - server now gets port from environment, python version now 3.5

--- code/app/server.py
+++ code/app/server.py
@@ -42,4 +42,5 @@ def upload_file():
         return jsonify(predictions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
